5d2a1bf35759d2bbb6694184faff0c9824590581
glay_luncy <ie925155@gmail.com>
2023-11-10 17:14:27 +0000

    construct matrix api skelton

--- src/hw1.cpp
+++ src/hw1.cpp
@@ -1,1 +1,88 @@
 #include "hw1.h"
+
+namespace algebra {
+    Matrix zeros(size_t n, size_t m)
+    {
+        return {};
+    }
+
+    Matrix ones(size_t n, size_t m)
+    {
+        return {};
+    }
+
+    Matrix random(size_t n, size_t m, double min, double max)
+    {
+        return {};
+    }
+
+    void show(const Matrix& matrix)
+    {
+
+    }
+
+    Matrix multiply(const Matrix& matrix, double c)
+    {
+        return {};
+    }
+
+    Matrix multiply(const Matrix& matrix1, const Matrix& matrix2)
+    {
+        return {};
+    }
+
+    Matrix sum(const Matrix& matrix, double c)
+    {
+        return {};
+    }
+
+    Matrix sum(const Matrix& matrix1, const Matrix& matrix2)
+    {
+        return {};
+    }
+
+    Matrix transpose(const Matrix& matrix)
+    {
+        return {};
+    }
+
+    Matrix minor(const Matrix& matrix)
+    {
+        return {};
+    }
+
+    double determinant(const Matrix& matrix)
+    {
+        return {};
+    }
+
+    Matrix inverse(const Matrix& matrix)
+    {
+        return {};
+    }
+
+    Matrix concatenate(const Matrix& matrix1, const Matrix& matrix2, int axis=0)
+    {
+        return {};
+    }
+
+    Matrix ero_swap(const Matrix& matrix, size_t r1, size_t r2)
+    {
+        return {};
+    }
+
+    Matrix ero_multiply(const Matrix& matrix, size_t r, double c)
+    {
+        return {};
+    }
+
+    Matrix ero_sum(const Matrix& matrix, size_t r1, double c, size_t r2)
+    {
+        return {};
+    }
+
+    Matrix upper_triangular(const Matrix& matrix)
+    {
+        return {};
+    }
+}
