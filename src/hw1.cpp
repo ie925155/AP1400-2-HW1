cab89165dec715c5468cd5a90455d69e56aef2a2
glay_luncy <ie925155@gmail.com>
2023-11-13 12:19:10 +0000

    Implement zeros, ones, random functions

--- src/hw1.cpp
+++ src/hw1.cpp
@@ -1,19 +1,72 @@
 #include "hw1.h"
 
+#include <random>
+
 namespace algebra {
+    /**
+     * Creates a matrix of zeros with the specified dimensions.
+     *
+     * @param n the number of rows in the matrix
+     * @param m the number of columns in the matrix
+     *
+     * @return a matrix of zeros with dimensions n x m
+     *
+     * @throws std::invalid_argument if n or m is less than or equal to 0
+     */
     Matrix zeros(size_t n, size_t m)
     {
-        return {};
+        if (n <= 0 || m <= 0) 
+            throw std::invalid_argument("n and m should be greater than 0");
+        return Matrix(n, std::vector<double>(m, 0.0));
     }
 
+    /**
+     * Generates a matrix filled with ones.
+     *
+     * @param n the number of rows in the matrix
+     * @param m the number of columns in the matrix
+     *
+     * @return a matrix of size n x m with all elements set to 1.0
+     *
+     * @throws std::invalid_argument if n or m is less than or equal to 0
+     */
     Matrix ones(size_t n, size_t m)
     {
-        return {};
+        if (n <= 0 || m <= 0) 
+            throw std::invalid_argument("n and m should be greater than 0");
+        return Matrix(n, std::vector<double>(m, 1.0));
     }
 
+    /**
+     * Generates a random matrix of size n x m with values between min and max.
+     *
+     * @param n the number of rows in the matrix
+     * @param m the number of columns in the matrix
+     * @param min the minimum value of the random number
+     * @param max the maximum value of the random number
+     *
+     * @return the randomly generated matrix
+     *
+     * @throws std::invalid_argument if n or m is less than or equal to 0
+     * @throws std::logic_error if min is greater than max
+     */
     Matrix random(size_t n, size_t m, double min, double max)
     {
-        return {};
+        if (n <= 0 || m <= 0) 
+            throw std::invalid_argument("n and m should be greater than 0");
+        if (min > max) {
+            throw std::logic_error("min cannot be greater than max");
+        }
+        Matrix matrix = zeros(n, m);
+
+        std::default_random_engine engine;
+        std::uniform_real_distribution<double> distribution(min, max);
+        for (auto& row : matrix) {
+            for (auto& elem : row) {
+                elem = distribution(engine);
+            }
+        }
+        return matrix;
     }
 
     void show(const Matrix& matrix)
